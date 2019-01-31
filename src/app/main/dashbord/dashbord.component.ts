63b3a65db0ec2f0808dccaf9c0fb02ec795fe72a
ajanieniolasolomon <eniolsolomon@gmail.com>
2019-01-31 19:51:28 +0100

    chart added

--- src/app/main/dashbord/dashbord.component.ts
+++ src/app/main/dashbord/dashbord.component.ts
@@ -16,6 +16,30 @@ export class DashbordComponent implements OnInit {
   form;
   chart = [];
   sucess;
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+  public radarChartLabels = ['Q1', 'Q2', 'Q3', 'Q4'];
+  public radarChartData = [
+    {data: [120, 130, 180, 70], label: '2017'},
+    {data: [90, 150, 200, 45], label: '2018'}
+  ];
+  public radarChartType = 'radar';
+  public radarChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public pieChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public pieChartData = [120, 150, 180, 90];
+  public pieChartType = 'pie';
   constructor(private service: ApiService, private fb: FormBuilder){}
 
   ngOnInit() {
@@ -39,7 +63,7 @@ onSubmit() {
   this.service.newUser(this.form.value).subscribe(res => {
 this.sucess = this.form.value.name + 'added';
 setTimeout(() => {
-  this.sucess = ''
+  this.sucess =  ''
 }, 4000 );
  this.form.reset();
   },
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,21 +4,21 @@ import { ClarityModule } from '@clr/angular';
 import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { ChartsModule } from 'ng2-charts';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ClarityModule,
     HttpClientModule,
-
+    ChartsModule,
     FormsModule, ReactiveFormsModule
   ],
   exports: [
     CommonModule,
     ClarityModule,
     HttpClientModule,
-
+    ChartsModule,
     FormsModule,
     ReactiveFormsModule
   ]
